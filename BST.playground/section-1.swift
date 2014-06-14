3c2acd8345a9364981db89f0f444ae3bb41cee00
Adam Cumiskey <adam.cumiskey@gmail.com>
2014-06-14 01:00:20 -0400

    Finished a few more of the problems

--- BST.playground/section-1.swift
+++ BST.playground/section-1.swift
@@ -5,6 +5,8 @@
 *  Adam Cumiskey 2014
 */
 
+import Foundation
+
 class BinarySearchTree {
     var root: Node?;
     
@@ -33,14 +35,14 @@ class BinarySearchTree {
     }
     
     /**
-     *  Helper method to lookup whether a given value exists in the tree
+    *  Helper method to lookup whether a given value exists in the tree
     **/
     func lookup(data: Int) -> Bool {
         return lookup(self.root!, data: data)
     }
     
     /**
-     *  Recursively search through the nodes for a value
+    *  Recursively search through the nodes for a value
     **/
     func lookup(node: Node?, data: Int) -> Bool {
         if node == nil {
@@ -58,15 +60,15 @@ class BinarySearchTree {
     }
     
     /**
-     *  Helper method to insert data into the tree
+    *  Helper method to insert data into the tree
     **/
     func insert(data: Int) {
         self.root = insert(self.root, data: data)
     }
     
     /**
-     *  Insert data into the tree by recursively finding the postition that will
-     *  keep the tree in order
+    *  Insert data into the tree by recursively finding the postition that will
+    *  keep the tree in order
     **/
     func insert(var node: Node!, data: Int) -> Node {
         if node == nil {
@@ -82,14 +84,14 @@ class BinarySearchTree {
     }
     
     /**
-     *  Helper method to calculate the total size of a tree
+    *  Helper method to calculate the total size of a tree
     **/
     func size() -> Int {
         return size(self.root?)
     }
     
     /**
-     *  Calculate the size of the nodes by recursively traversing the tree
+    *  Calculate the size of the nodes by recursively traversing the tree
     **/
     func size(node: Node?) -> Int {
         if node == nil {
@@ -100,14 +102,14 @@ class BinarySearchTree {
     }
     
     /**
-     *  Helper method to calculate the max depth of the tree
+    *  Helper method to calculate the max depth of the tree
     **/
     func maxDepth() -> Int {
         return maxDepth(self.root)
     }
     
     /**
-     *  Recursively calculate the max depth from a given node
+    *  Recursively calculate the max depth from a given node
     **/
     func maxDepth(node: Node?) -> Int {
         if node == nil {
@@ -125,15 +127,15 @@ class BinarySearchTree {
     }
     
     /**
-     *  Helper function to calculate the minimum value
+    *  Helper function to calculate the minimum value
     **/
     func minValue() -> Int? {
         return minValue(self.root?)
     }
     
     /**
-     *  Get the minimum value by traversing all the left nodes in the tree until
-     *  a leaf is reached
+    *  Get the minimum value by traversing all the left nodes in the tree until
+    *  a leaf is reached
     **/
     func minValue(node: Node?) -> Int? {
         var currentNode = node
@@ -144,15 +146,15 @@ class BinarySearchTree {
     }
     
     /**
-     *  Helper function to calculate the maximum value
+    *  Helper function to calculate the maximum value
     **/
     func maxValue() -> Int? {
         return maxValue(self.root?)
     }
     
     /**
-     *  Get the maximum value by traversing all the right nodes in the tree until
-     *  a leaf is reached
+    *  Get the maximum value by traversing all the right nodes in the tree until
+    *  a leaf is reached
     **/
     func maxValue(node: Node?) -> Int? {
         var currentNode = node
@@ -161,13 +163,91 @@ class BinarySearchTree {
         }
         return currentNode?.data
     }
+    
+    /**
+    *  Helper function to print the tree in acending order
+    **/
+    func printTree() {
+        printTree(self.root?)
+    }
+    
+    /**
+    *  Recursively print the tree starting from a given node
+    **/
+    func printTree(node: Node?) {
+        if node == nil {
+            return;
+        }
+        
+        if let left = node?.left? {
+            printTree(left)
+        }
+        
+        println("\(node?.data?)")
+        
+        if let right = node?.right? {
+            printTree(right)
+        }
+    }
+    
+    /**
+    *  Helper function to print the tree in postorder
+    **/
+    func printPostorder() {
+        printPostorder(self.root?)
+    }
+    
+    /**
+    *  Print the in postorder starting from a given node
+    **/
+    func printPostorder(node: Node?) {
+        if node == nil {
+            return;
+        }
+        
+        if let left = node?.left? {
+            printTree(left)
+        }
+        
+        if let right = node?.right? {
+            printTree(right)
+        }
+        
+        println("\(node?.data?)")
+    }
+    
+    /**
+    *  Helper function to find whether or not a tree contains an input path sum
+    **/
+    func hasPathSum(sum: Int) -> Bool {
+        return hasPathSum(self.root?, sum: sum)
+    }
+    
+    /**
+    *  Check to see if there is a path sum starting at a given node
+    **/
+    func hasPathSum(node: Node?, var sum: Int) -> Bool {
+        if node? == nil {
+            return sum == 0;
+        } else {
+            if let data = node?.data? {
+                sum -= data
+            }
+            return (hasPathSum(node?.left?, sum: sum) ||
+                hasPathSum(node?.right?, sum: sum))
+        }
+    }
 }
 
 // 1. build123()
 var build123 = BinarySearchTree()
+build123.insert(12)
 build123.insert(2)
 build123.insert(1)
+build123.insert(32)
+build123.insert(55)
 build123.insert(3)
+build123.insert(4)
 
 println("-- #1 --")
 println("build123()")
@@ -181,10 +261,10 @@ println("size()")
 println("The build123 tree has \(build123.size()) nodes \n")
 
 // 3. maxDepth()
-build123.insert(12)
 build123.insert(19)
 build123.insert(21)
-build123.insert(3)
+build123.insert(5)
+build123.insert(9)
 
 println("-- #3 --")
 println("maxDepth()")
@@ -193,9 +273,31 @@ println("MaxDepth of build123 tree is \(build123.maxDepth()) \n")
 // 4. minValue()
 println("-- #4 --")
 println("minValue()")
-println("The build123 tree has a minValue of \(build123.minValue())")
+println("The build123 tree has a minValue of \(build123.minValue())\n")
 
 // 5. maxValue()
 println("-- #5 --")
 println("maxValue()")
-println("The build123 tree has a maxValue of \(build123.maxValue())")
+println("The build123 tree has a maxValue of \(build123.maxValue())\n")
+
+// 6. printTree()
+println("-- #6 --")
+println("printTree()")
+build123.printTree()
+
+// 7. printPostorder()
+println("\n-- #7 --")
+println("printPostorder()")
+build123.printPostorder()
+
+// 8. hasPathSum()
+println("\n-- #8 --")
+println("hasPathSum()")
+
+for i in 0..100 {
+    let hasPathSum = build123.hasPathSum(i)
+    if hasPathSum {
+        println("Path sum in tree for \(i)")
+
+    }
+}
